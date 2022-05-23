ef6d59b944496cc1523fd4bdd59527113cd56065
jaewon2336 <xldzjqpf1588@naver.com>
2022-05-23 14:45:21 +0900

    주소찾기 - 스토어 사용 로직

--- src/main/java/site/metacoding/jusotest/JusoController.java
+++ src/main/java/site/metacoding/jusotest/JusoController.java
@@ -4,20 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class JusoController {
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String home(Model model) {
         String jusoUrl = "https://www.juso.go.kr/addrlink/addrLinkUrl.do?confmKey=devU01TX0FVVEgyMDIyMDUyMzEwNDgzNTExMjYwMDA=&returnUrl=http://localhost:8080/juso/callback&resultType=4";
         model.addAttribute("jusoUrl", jusoUrl);
-        return "/index";
+        return "/home";
     }
 
+    // 해당 콜백이 호출되면 push 해주는 것이 좋다. 스트림이 연결되어 있어야 한다.(ex. 웹소켓, SSE 프로토콜)
     @PostMapping("/juso/callback")
     public void jusoCallback(String roadFullAddr) {
         System.out.println(roadFullAddr);
+        Store.roadFullAddr = roadFullAddr;
+    }
+
+    @GetMapping("/juso/check")
+    public @ResponseBody String jusoCheck() {
+        return Store.roadFullAddr;
     }
 
 }
---
+++ src/main/java/site/metacoding/jusotest/Store.java
@@ -0,0 +1,6 @@
+package site.metacoding.jusotest;
+
+// 모든 클라이언트가 얘를 공유하게 되기 때문에 Redis(키:밸류)를 사용해야 한다. 혹은 세션 저장소
+public class Store {
+    public static String roadFullAddr;
+}
